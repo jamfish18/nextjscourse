b7b007804588273eddf15a65e847f885afc98bb2
James <james.owen.turner@gmail.com>
2025-01-30 14:12:33 +1100

    up to chapter 10, pagination

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig = {
-  experimental: {
-    ppr: 'incremental',
-  },
+  // experimental: {
+  //   ppr: 'incremental',
+  // },
 };
 
 export default nextConfig;
