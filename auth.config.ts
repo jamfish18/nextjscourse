72293c97bc21789b70aceee70ed7f8b200efb1e8
James <james.owen.turner@gmail.com>
2025-02-03 10:02:14 +1100

    routing changes for readability

--- auth.config.ts
+++ auth.config.ts
@@ -4,6 +4,8 @@
 
 import type { NextAuthConfig } from 'next-auth';
  
+// exports an authConfig object
+// object contains the configuration options for NextAuth.js
 export const authConfig = {
   pages: {
     signIn: '/login', // redirect to our customer login page on signin
@@ -12,7 +14,7 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      if (isOnDashboard) {
+      if (isOnDashboard) { // if the user is on the dashboard
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
